379577ea757e1947c9a18d40317daa65077c2b24
Klaudia Kurela <klakure97@gmail.com>
2022-09-20 23:32:35 +0200

    Exception handling added

--- ZadaniePESEL/Controllers/PeselController.cs
+++ ZadaniePESEL/Controllers/PeselController.cs
@@ -17,54 +17,84 @@ namespace ZadaniePESEL.Controllers
 
         //Usługa 1.
         [HttpGet("GetAge/{pesel}", Name = "Wiek")]
-        //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
         public IActionResult GetAge(string pesel)
         {
-            if (pesel is null || pesel == string.Empty)
+            try
             {
-                return NotFound();
-            }
+                if (pesel is null || pesel == string.Empty)
+                {
+                    return NotFound();
+                }
 
-            if (!_peselService.PeselValidation(pesel))
+                if (!_peselService.PeselValidation(pesel))
+                {
+                    return BadRequest();
+                }
+
+                return Ok(_peselService.Age(pesel));
+            }
+            catch
             {
-                return BadRequest();
+                return Problem();
             }
-
-            return Ok(_peselService.Age(pesel));
         }
 
         //Usługa 2.
         [HttpGet("GetPromotion/{pesel}", Name = "Promocja")]
         public IActionResult GetPromotion(string pesel)
         {
-            if (pesel is null || pesel == string.Empty)
+            try
             {
-                return NotFound();
-            }
+                if (pesel is null || pesel == string.Empty)
+                {
+                    return NotFound();
+                }
 
-            if (!_peselService.PeselValidation(pesel))
+                if (!_peselService.PeselValidation(pesel))
+                {
+                    return BadRequest();
+                }
+
+                return Ok(_peselService.Age(pesel));
+            }
+            catch
             {
-                return BadRequest();
+                return Problem();
             }
-
-            return Ok(_peselService.Promotion(pesel));
         }
 
         //Usługa 3.
         [HttpGet("GetWishes/{pesel}/{name}/{surname}", Name = "Życzenia")]
         public IActionResult GetWishes(string pesel, string name, string surname)
         {
-            if (pesel is null || pesel == string.Empty)
+            try
             {
-                return NotFound();
-            }
+                if (pesel is null || pesel == string.Empty)
+                {
+                    return NotFound();
+                }
 
-            if (!_peselService.PeselValidation(pesel))
+                if (!_peselService.PeselValidation(pesel))
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (name is null || name == string.Empty)
+                {
+                    return NotFound();
+                }
+
+                if (surname is null || surname == string.Empty)
+                {
+                    return NotFound();
+                }
+
+                return Ok(_peselService.Wishes(pesel, name, surname));
+            }
+            catch
             {
-                return BadRequest(ModelState);
+                return Problem();
             }
-
-            return Ok(_peselService.Wishes(pesel, name, surname));
         }
     }
 }
